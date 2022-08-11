286fe3096ed1a1195e84e40fd31e8be874acf042
Limor Segal Shevah <110296653+limorss@users.noreply.github.com>
2022-08-11 20:05:47 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -3,7 +3,8 @@ def save_name(names_file):
     current_name = input("Enter your name: ")
     my_file.write(current_name + "\n")
     my_file.close()
-
+def haim(avi):
+    print(avi)
 def show_names(names_file, blabla):
     my_file = open(names_file, "r")  # add in the end
     for name in my_file.readlines():
